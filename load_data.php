9924325004f2bcc1b0bef8c0aa1b2bec53321d9a
csynoers <3s0c9m7@gmail.com>
2019-02-22 16:17:51 +0700

    second commit

--- load_data.php
+++ load_data.php
@@ -1,8 +1,51 @@
 <?php
+    // create connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "datatables";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        die();
+    }
+    // end create connection
+    
+    $records_total = 1000;
+
+    $query= "SELECT * FROM tes";
+    $query .= $_POST['length'] != -1 ? $_POST['start'].','.$_POST['length'] : null;
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $records_filtered = $statement->rowCount();
+
+    // create variabel for save data from push with foreach
+    $data= []; 
+    // ceate number
+    $no = $_POST['start'];
+    
+    foreach($result as $row)
+    {
+        $data[] = [
+            $no,
+            $row['first_name'],
+            $row['last_name'],
+            'Action'
+        ];
+        $no++;
+    }
+
     $output = array(
-        "draw"				=>	intval($_POST["draw"]),
-        "recordsTotal"		=> 	$filtered_rows,
-        "recordsFiltered"	=>	$totalRow,
-        "data"				=>	$data
+        // "draw"				=>	intval($_POST["draw"]),
+        "draw"				=>	$_POST["draw"],
+        "recordsTotal"		=> 	$records_filtered,
+        "recordsFiltered"	=>	$records_filtered,
+        "data"				=>	$data,
+        "console_log"       => $result
     );
     echo json_encode($output);
+    // print_r($_REQUEST);
