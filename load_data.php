11744ec41d2a301e541372108f46df2d67b974e4
csynoers <3s0c9m7@gmail.com>
2019-02-22 19:31:02 +0700

    Fix & code clean

--- load_data.php
+++ load_data.php
@@ -14,19 +14,59 @@
     }
     // end create connection
     
-    $records_total = 1000;
+    $records_total  = 1000;
 
-    $query= "SELECT * FROM tes";
-    $query .= $_POST['length'] != -1 ? $_POST['start'].','.$_POST['length'] : null;
-    $statement = $conn->prepare($query);
+    # default query
+    $query          = "SELECT * FROM tes WHERE 1=1 ";
+
+    # set column field database for datatable orderable
+    $column_order   = [
+        'id',
+        'first_name',
+        'last_name',
+    ];
+    
+    # set column field database for datatable searchable 
+    $column_search  = [
+        'first_name',
+        'last_name',
+    ]; 
+	
+    # generate like condition for search
+    $like           = '';
+    $i              = 0;
+    foreach ($column_search as $item)
+    { # loop column
+        if ($_POST['search']['value']) { # if datatable send POST for search
+            $like .= $i==0 ? ' AND (' : null ;
+            $like .= $item.' LIKE \'%'.str_replace("'", "\'", $_POST['search']['value']).'%\'';
+            $like .= count($column_search) - 1 == $i ? ') ' : ' OR ';
+        }else{ # if datatable not send POST for search
+            $like .='';
+        }
+        $i++;
+    }
+    $query .= $like;
+    # end generate like condition for search
+
+    # sorting
+    $query .= isset($_POST["order"])? 'ORDER BY '.$column_order[$_POST['order'][0]['column']].' '.$_POST['order']['0']['dir'] : 'ORDER BY id '; 
+    # end sorting
+
+    # limit
+    $limit = $_POST['length'] != -1 ? $_POST['start'].','.$_POST['length'] : null;
+    # end limit
+
+    # collect data from tables (PDO)
+    $statement = $conn->prepare("$query LIMIT $limit" );
     $statement->execute();
     $result = $statement->fetchAll();
     $records_filtered = $statement->rowCount();
 
-    // create variabel for save data from push with foreach
+    # create variabel for save data from push with foreach
     $data= []; 
-    // ceate number
-    $no = $_POST['start'];
+    # ceate number
+    $no = ($_REQUEST["start"]==0) ? 1 : $_REQUEST["start"] ;
     
     foreach($result as $row)
     {
@@ -42,10 +82,9 @@
     $output = array(
         // "draw"				=>	intval($_POST["draw"]),
         "draw"				=>	$_POST["draw"],
-        "recordsTotal"		=> 	$records_filtered,
-        "recordsFiltered"	=>	$records_filtered,
+        "recordsTotal"		=> 	$records_total,
+        "recordsFiltered"	=>	$records_total,
         "data"				=>	$data,
-        "console_log"       => $result
+        "console_log"       => $query
     );
     echo json_encode($output);
-    // print_r($_REQUEST);
